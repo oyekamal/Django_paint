656dabb1b64d2bfebde1d4e48b9e84f2533ec01c
oykamal <autocratickamal1@gmail.com>
2022-04-24 17:59:01 +0500

    added comment into view

--- core/paint/views.py
+++ core/paint/views.py
@@ -7,6 +7,7 @@ from .models import Files
 from django.views.decorators.csrf import csrf_exempt
 import json
 import random
+from random import shuffle
 
 import string
 
@@ -14,8 +15,10 @@ import string
 @csrf_exempt
 def paint(request):
     if request.method == 'GET':
+        # when  GET method is called from the HTML this part of  code will run and render paint.html
         return render(request, 'paint.html')
     elif request.method == 'POST':
+        # when the user press on the save butthon this part of code runs and save the canves details into db
         filename = request.POST['save_fname']
         data = request.POST['save_cdata']
         image = request.POST['save_image']
@@ -27,6 +30,7 @@ def paint(request):
 @csrf_exempt
 def files(request):
     if request.method == 'GET':
+        # when user click on the history butthon all the save paint letter are going to shown to on the files.html file
         all_data = Files.objects.all()
         return render(request, 'files.html', {'files': all_data})
 
@@ -34,38 +38,43 @@ def files(request):
 @csrf_exempt
 def game(request):
     if request.method == 'GET':
-        # all_data = Files.objects.all()
+        """ in this part of code i have writen the login of game. """
 
+        # first step write all ABC. A to Z
         ABC = string.ascii_uppercase
+
+        # convert string ABC into list ABC for example ['A','B','C']
         list_of_ABC = list(ABC)
 
-        # sampling with replacement
-        # k = number of items to select
+        # pick 1 rendom letter from the list of ABC list
         question_letter = random.choices(list_of_ABC, k=1)
+
+        # pick 3 rendom letter from the list of ABC list.
         sample_list = random.choices(list_of_ABC, k=3)
+
+        # append the question Letter into the choices list.
         sample_list.append(question_letter[0])
-        print(question_letter)
-        print(sample_list)
+
+        # Now this part is important i have added extra string to the ABC use this pattern i can access each ABC images in HTML
         question_letter = "alphabets/{}_500x500.png".format(
             question_letter[0])
         sample_list = ["alphabets/{}_500x500.png".format(
             each_sample) for each_sample in sample_list]
+
+        # this part shuffle the list
+        shuffle(sample_list)
         sample_list = enumerate(sample_list)
 
-        print(question_letter)
-        print(sample_list)
         return render(request, 'game.html', {"question_letter": question_letter, "sample_list": sample_list})
+
 
 @csrf_exempt
 def game_2(request):
     if request.method == 'GET':
-        # all_data = Files.objects.all()
 
         ABC = string.ascii_uppercase
         list_of_ABC = list(ABC)
 
-        # sampling with replacement
-        # k = number of items to select
         question_letter = random.choices(list_of_ABC, k=1)
         sample_list = random.choices(list_of_ABC, k=3)
         sample_list.append(question_letter[0])
@@ -84,8 +93,8 @@ def game_2(request):
 
 def search(request):
     if 'filename' in request.GET:
+        # this part of code search the file in db and send it to search.HTML file
         filename = request.GET['filename']
         datafile = Files.objects.filter(name=filename)
         count = Files.objects.filter(name=filename).count()
-        # print(datafile.values_list())
         return render(request, 'search.html', {'data': datafile, 'filename': filename, 'count': count})
